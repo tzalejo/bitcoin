9408099e5c33e7c73d48588127cba1981e77c70c
talejo <tzalejo@gmail.com>
2020-04-07 17:59:24 -0300

    servicios para las api

--- src/app/core/services/bitstamp/bitstamp.service.ts
+++ src/app/core/services/bitstamp/bitstamp.service.ts
@@ -1,11 +1,48 @@
 import { Injectable } from '@angular/core';
+import { environment } from '@environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from '../interceptor/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BitstampService {
+  // tslint:disable-next-line: variable-name
+  url_api_Bitstamp = environment.url_api_bitstamp;
+  header = this.authService.httpOptions;
+  transactions = 'https://www.bitstamp.net/api/v2/transactions/'
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService
+  ) {}
 
-  constructor() {
-    
+  // getBitcoin(): Observable<any> {
+  //   // return this.http.get<any>(`https://www.bitstamp.net/api/ticker/`, httpOptions);
+  //   return this.http.get<any>(`${this.url_api_Bitstamp}/btcusd`, this.header);
+  // }
+  // getEthereum(): Observable<any> {
+  //   return this.http.get<any>(`${this.url_api_Bitstamp}/ethusd`, this.header);
+  // }
+  // getLitecoin(): Observable<any> {
+  //   return this.http.get<any>(`${this.url_api_Bitstamp}/ltcusd`, this.header);
+  // }
+
+  // datos de la evolucion de la moneda con el formato:
+  // [{
+  //     "date": "1586120810",
+  //     "tid": "110046553",
+  //     "price": "6754.05",
+  //     "type": "0",
+  //     "amount": "0.08984875"
+  // }]....
+  getBitcoinTransactions(): Observable<any> {
+    return this.http.get(`${this.transactions}/btcusd?day`);
+  }
+  getEthereumTransactions(): Observable<any> {
+    return this.http.get(`${this.transactions}/ethusd?day`);
+  }
+  getLitecoinTransactions(): Observable<any> {
+    return this.http.get(`${this.transactions}/ltcusd?day`);
   }
 }
--- src/app/core/services/coinmarketcap/coinmarketcap.service.ts
+++ src/app/core/services/coinmarketcap/coinmarketcap.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
-
+import { environment } from '@environments/environment';
+import { AuthService } from '../interceptor/auth.service';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class CoinmarketcapService {
+  // tslint:disable-next-line: variable-name
+  url_api_coinmarket = environment.url_api_coinmarketcap;
+  header  = this.authService.httpOptions;
+  keyConmarket = 'CMC_PRO_API_KEY=b5cbb9c6-e173-4ec2-ad59-dfb960066ac2';
+  constructor(
+    private authService: AuthService,
+    private httpClient: HttpClient
+  ) {
+  }
 
-  constructor() { }
+  // historico  
+  // https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/historical 
+
+
+  
+  getCoinmarket(): Observable<any> {
+    return this.httpClient.get(`${this.url_api_coinmarket}?${this.keyConmarket}`, this.header);
+  }
+
 }
