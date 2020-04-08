797fb02935fee05105de5d51299869b7b201ef02
talejo <tzalejo@gmail.com>
2020-04-08 16:45:39 -0300

    agrego lo logout

--- src/app/layout/components/layout.component.ts
+++ src/app/layout/components/layout.component.ts
@@ -1,20 +1,30 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { map, shareReplay } from 'rxjs/operators';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { AuthService } from '@core/services/interceptor/auth.service';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent {
-
   // isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
   //   .pipe(
   //     map(result => result.matches),
   //     shareReplay()
   //   );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private authSevice: AuthService,
+    private router: Router,
+  ) {}
+
+  logout() {
+    this.authSevice.logout();
+    this.router.navigate(['']);
+  }
 
 }
