92d8275e125ebb357f33236e0c3f31a52170d06f
talejo <tzalejo@gmail.com>
2020-04-10 19:53:53 -0300

    quito algunos headers que generaban error

--- src/app/core/services/interceptor/auth.service.ts
+++ src/app/core/services/interceptor/auth.service.ts
@@ -72,8 +72,6 @@ export class AuthService {
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': 'true'
       })
     };
     return httpOptions;
