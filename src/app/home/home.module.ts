ffc0e7de6a4ae70f30a475655fff2f83daa0528c
talejo <tzalejo@gmail.com>
2020-04-16 16:54:31 -0300

    agrego el componente filtro al module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 // material
 import { MaterialModule } from '@material/material.module';
 // componentes
@@ -8,19 +10,21 @@ import { ProveComponent } from './components/prove/prove.component';
 import { HomeComponent } from './components/home/home.component';
 import { ClienteComponent } from './components/cliente/cliente.component';
 import { HighchartsChartModule } from 'highcharts-angular';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FiltrosComponent } from './components/filtros/filtros.component';
 @NgModule({
   declarations: [
     HomeComponent,
     ProveComponent,
     ClienteComponent,
+    FiltrosComponent,
   ],
   imports: [
     CommonModule,
     MaterialModule,
     HomeRoutingModule,
     HighchartsChartModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ]
 })
 export class HomeModule { }
