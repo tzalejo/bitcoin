28fd6bf12c13d2db69424081a24afd20f1f6ec79
talejo <tzalejo@gmail.com>
2020-04-16 16:51:26 -0300

    agrego metodo para obtener formulario con filtro

--- src/app/core/services/formulario/formulario.service.ts
+++ src/app/core/services/formulario/formulario.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 import { environment } from '@environments/environment';
 import { AuthService } from '../interceptor/auth.service';
@@ -19,7 +19,27 @@ export class FormularioService {
   ) {}
 
   getFormularios(): Observable<any[]> {
-    return this.httpCliente.get<any []>(`${this.url}`, this.header);
+    return this.httpCliente.get<any[]>(`${this.url}`, this.header);
+  }
+  getFormulariosFilter(filter): Observable<any> {
+    const compraMoneda: string = (filter.compra_moneda === null ||
+                                  typeof filter.compra_moneda === 'undefined'  ) ? '' : filter.compra_moneda;
+    const tipoCriptomoneda: string = (filter.tipo_criptomoneda === null ||
+                                  typeof filter.tipo_criptomoneda === 'undefined'  ) ? '' : filter.tipo_criptomoneda;
+    const fechaDesde: string = (filter.fechaDesde === null ||
+                                  typeof filter.fechaDesde === 'undefined'  ) ? '' : filter.fechaDesde;
+    const fechaHasta: string = (filter.fechaHasta === null ||
+                                  typeof filter.fechaHasta === 'undefined'  ) ? '' : filter.fechaHasta;
+
+    const estado: string = (filter.estado === null || typeof filter.estado === 'undefined'  ) ? '' : filter.estado;
+    const cliente: string = (filter.cliente === null || typeof filter.cliente === 'undefined'  ) ? '' : filter.cliente;
+    return this.httpCliente.get(`${this.url}?
+            compra_moneda=${compraMoneda}&
+            tipo_criptomoneda=${tipoCriptomoneda}&
+            fechaDesde=${fechaDesde}&
+            fechaHasta=${fechaHasta}&
+            cliente=${cliente}&
+            estado=${estado}`, this.header);
   }
 
   crearFormulario(formulario: Formulario): Observable<any> {
