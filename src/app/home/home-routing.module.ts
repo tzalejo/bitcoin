7e94483b52b2187d2b3b20e867b6e7a047a5aed1
talejo <tzalejo@gmail.com>
2020-04-16 16:51:54 -0300

    ruta para filtro

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ProveComponent } from './components/prove/prove.component';
 import { ClienteComponent } from './components/cliente/cliente.component';
+import { FiltrosComponent } from './components/filtros/filtros.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'cliente', component: ClienteComponent},
   {path: 'prove', component: ProveComponent},
+  {path: 'filtros', component: FiltrosComponent},
 ];
 
 @NgModule({
